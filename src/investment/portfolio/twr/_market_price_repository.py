02c96dbe7a579c0e0831c73650e09fd63abd6e0d
Rui Xue <ruixue.fi@gmail.com>
2026-09-01 21:40:40 +0300

    #32 MarketPriceRepository added

--- src/investment/portfolio/twr/_market_price_repository.py
+++ src/investment/portfolio/twr/_market_price_repository.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
+from datetime import date, datetime
 from investment.marketquote.repository import fetch_historical_prices, fetch_price_in_euro
 from investment.util.constants import EUR
 from investment.vo.value_objects import Period, Price, PriceSeries
 
-
-def find_historical_euro_price_series(security_id:str, period:Period) -> PriceSeries:
+def _find_historical_euro_price_series(security_id:str, period:Period) -> PriceSeries:
     price_series = fetch_historical_prices(security_id, period)
 
     euro_cent_prices = {
@@ -19,3 +17,15 @@ def find_historical_euro_price_series(security_id:str, period:Period) -> PriceSe
         for trading_date, cent_value in price_series.cent_prices.items()
     }
     return PriceSeries(currency=EUR, cent_prices=euro_cent_prices)
+
+class MarketPriceRepository:
+    def __init__(self, end_date: date) -> None:
+        self.end_date = end_date
+        self.series_cache: dict[str, PriceSeries] = {}
+    def find_euro_price(self, security_id:str, date:date) -> Price:
+        price_series = self.series_cache.get(security_id)
+        if price_series is None:
+            period = Period(from_date=date, to_date=self.end_date)
+            price_series = _find_historical_euro_price_series(security_id, period)
+            self.series_cache[security_id] = price_series
+        return price_series.get_price(date)
