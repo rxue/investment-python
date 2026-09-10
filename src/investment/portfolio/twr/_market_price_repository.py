007d8cd53d4f2f62c252824bdb759286defe72c1
Rui Xue <ruixue.fi@gmail.com>
2026-09-10 21:22:12 +0300

    #43 cleaned _market_price_repository.py

--- src/investment/portfolio/twr/_market_price_repository.py
+++ src/investment/portfolio/twr/_market_price_repository.py
@@ -9,29 +9,6 @@ from investment.util.util import EUR, convert_to_euro_cent
 from investment.vo.value_objects import FxRateSeries, Period, Price, PriceSeries
 
 _fx_rate_series_cache:dict[str,FxRateSeries] = dict()
-def _find_historical_euro_price_series(security_id:str, period:Period) -> PriceSeries:
-    price_series = fetch_historical_prices(security_id, period)
-    def fetch_price_in_euro(existing_price: Price) -> Price:
-        currency: Final = existing_price.currency
-        if currency == EUR:
-            return existing_price
-        else:
-            if currency not in _fx_rate_series_cache:
-                _fx_rate_series_cache[currency] = fetch_fx_rate_series_from_euro(currency, period)
-            fx_rate = _fx_rate_series_cache[currency].get(existing_price.date())
-            price_value_in_euro = convert_to_euro_cent(existing_price, fx_rate)
-            return Price(price_value_in_euro, EUR, existing_price.timestamp)
-    euro_cent_prices = {
-        trading_date: fetch_price_in_euro(
-            Price(
-                cent_value,
-                price_series.currency,
-                datetime.combine(trading_date, datetime.min.time()),
-            )
-        ).cent_value
-        for trading_date, cent_value in price_series.cent_prices.items()
-    }
-    return PriceSeries(currency=EUR, cent_prices=euro_cent_prices)
 
 class MarketPriceRepository:
     def __init__(self, end_date: date) -> None:
